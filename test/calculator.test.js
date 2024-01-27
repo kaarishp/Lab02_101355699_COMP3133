bdf61e8f09e6ad22091380c95f712437e6b437bf
Kaarish <kingkaarish@gmail.com>
2024-01-27 18:01:52 -0500

    testing updated

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -3,45 +3,37 @@ const calculator = require('../app/calculator');
 
 describe("Calculator Tests", () => {
     // Addition Tests
-    it('add', function () {
+    it('add(5, 2) expected result 7 - PASS', function () {
         assert.equal(calculator.add(5, 2), 7);
-        console.log('add(5, 2) expected result 7 - PASS');
     });
-    it('add', function () {
+    it('add(5, 2) expected result 8 - FAIL', function () {
         assert.equal(calculator.add(5, 2), 8); 
-        console.log('add(5, 2) expected result 8 - FAIL');
     });
 
     // Subtraction Tests
-    it('sub', function () {
+    it('sub(5, 2) expected result 3 - PASS', function () {
         assert.equal(calculator.sub(5, 2), 3); 
-        console.log('sub(5, 2) expected result 3 - PASS');
     });
 
-    it('sub', function () {
+    it('sub(5, 2) expected result 5 - FAIL', function () {
         assert.equal(calculator.sub(5, 2), 5); 
-        console.log('sub(5, 2) expected result 5 - FAIL');
     });
 
     // Multiplication Tests
-    it('mul', function () {
+    it('mul(5, 2) expected result 10 - PASS', function () {
         assert.equal(calculator.mul(5, 2), 10); 
-        console.log('mul(5, 2) expected result 10 - PASS');
     });
 
-    it('mul', function () {
+    it('mul(5, 2) expected result 12 - FAIL', function () {
         assert.equal(calculator.mul(5, 2), 12);
-        console.log('mul(5, 2) expected result 12 - FAIL');
     });
     
     // Division Tests
-    it('div', function () {
+    it('div(10, 2) expected result 5 - PASS', function () {
         assert.equal(calculator.div(10, 2), 5);
-        console.log('div(10, 2) expected result 5 - PASS');
     });
 
-    it('div', function () {
+    it('div(10, 2) expected result 2 - FAIL', function () {
         assert.equal(calculator.div(10, 2), 2);
-        console.log('\tdiv(10, 2) expected result 2 - FAIL');
     });
 });
